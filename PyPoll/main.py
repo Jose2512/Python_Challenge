b6995b54c63fcbb1ade672b08f808e942564853d
José Alvarez <josealvarezhuerta@me.com>
2020-03-01 12:57:12 -0600

    PyBank done but could be improved

---
+++ PyPoll/main.py
@@ -0,0 +1,35 @@
+import csv
+
+election_data = "election_data.csv"
+voter_counter = 0
+with open (election_data, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csv_header = next(csvfile)
+    for row in csvreader:
+        voter_counter += 1
+       # total_PL += int(row[1])
+        #if int(row[1]) < greatest_dec:
+         #   greatest_dec = int(row[1])
+          #  greatest_dec_m = row[0]
+        #if int(row[1]) > greatest_inc:
+         #   greatest_inc = int(row[1])
+          #  greatest_inc_m = row[0]
+
+#print(f"""Financial Analysis
+#-------------------------------------   
+#Total months: {month_counter}
+#Total: ${total_PL}
+#Average change: ${round(total_PL/month_counter,2)}
+#Greatest Increase in Profits: {greatest_dec_m} (${greatest_dec})
+#Greatest Decrease in Profits: {greatest_inc_m} (${greatest_inc})
+#""")
+#output_file = open("Financial_Analysis.txt", "w+")
+#output_file.write (f"""Financial Analysis
+#-------------------------------------   
+#Total months: {month_counter}
+#Total: ${total_PL}
+#Average change: ${round(total_PL/month_counter,2)}
+#Greatest Increase in Profits: {greatest_dec_m} (${greatest_dec})
+#Greatest Decrease in Profits: {greatest_inc_m} (${greatest_inc})
+#""")
+print(voter_counter)
