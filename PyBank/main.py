eb05dbd5e580cca1a3c69cf11d909b08149afb58
José Alvarez <josealvarezhuerta@me.com>
2020-03-01 19:13:52 -0600

    Homework finished without validation

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,37 +1,41 @@
 import csv
-
+# Set a variable for the file to be read
 budgetdata = "budget_data.csv"
+# Variable to count the months 
 month_counter = 0
+#Variable to sum all PL 
 total_PL = 0
-greatest_inc = 0
-greatest_dec = 0
+#list for profits and months
+profit = []
+months = []
 with open (budgetdata, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    # Pass the headers row
     csv_header = next(csvfile)
     for row in csvreader:
+        # adding 1 number to the counter for each month
         month_counter += 1
+        # adding the profit value to the variable. Since losses have "-" a sum will work
         total_PL += int(row[1])
-        if int(row[1]) < greatest_dec:
-            greatest_dec = int(row[1])
-            greatest_dec_m = row[0]
-        if int(row[1]) > greatest_inc:
-            greatest_inc = int(row[1])
-            greatest_inc_m = row[0]
+        # Add the P&L value to a list and the months to another list
+        profit.append(int(row[1]))
+        months.append(row[0])
 
-print(f"""Financial Analysis
+# Search for the highest number in the list and return its index
+index_increase = profit.index(max(profit))
+# Search for the lowest number in the list and return its index
+index_decrease = profit.index(min(profit))
+# The index is used to find value of profit and month in the lists
+
+fin_analysis = f"""Financial Analysis
 -------------------------------------   
 Total months: {month_counter}
-Total: ${total_PL}
-Average change: ${round(total_PL/month_counter,2)}
-Greatest Increase in Profits: {greatest_dec_m} (${greatest_dec})
-Greatest Decrease in Profits: {greatest_inc_m} (${greatest_inc})
-""")
+Total: ${total_PL:,}
+Average change: ${round(total_PL/month_counter,2):,}
+Greatest Increase in Profits: {months[index_increase]} (${profit[index_increase]:,})
+Greatest Decrease in Profits: {months[index_decrease]} (${profit[index_decrease]:,})
+"""
+print(fin_analysis)
+# Crate file and write the financial analysis 
 output_file = open("Financial_Analysis.txt", "w+")
-output_file.write (f"""Financial Analysis
--------------------------------------   
-Total months: {month_counter}
-Total: ${total_PL}
-Average change: ${round(total_PL/month_counter,2)}
-Greatest Increase in Profits: {greatest_dec_m} (${greatest_dec})
-Greatest Decrease in Profits: {greatest_inc_m} (${greatest_inc})
-""")
+output_file.write (fin_analysis)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,37 +1,60 @@
 import csv
-loope=0
+# Declaring lists for candidates, votes and percentages
 candidates = []
 votes = []
 percentages = []
+# Set a variable for the file to be read
 election_data = "election_data.csv"
+# Setting a counter for the voters
 voter_counter = 0
 with open (election_data, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+     # Pass the headers row
     csv_header = next(csvfile)
     for row in csvreader:
+        # adding 1 number to the counter for each row (voter)
         voter_counter += 1
+        # If the name of the candidate (row[2]) already exist in the list of candidates, do nothing, else add it to the list
         if row[2] in candidates:
             pass
         else:
             candidates.append(row[2])
+            # also creates a space with value 0 in the votes list for each candidate
             votes.append(0)
+        # for each row, verifies the canddidate name and return its index value from the candidates list
         index = candidates.index(row[2])
+        # add a value of 1 in the votes list in the index value determined for the candidate
         votes[index] +=1
-    for x in candidates:
-        #percentages.append(round(votes[loope]/voter_counter*100,2))
-        percentages.append(votes[loope]/voter_counter)
-        loope += 1
+    # for each candidate, it will calculate its percentage and add it to another list
+    for x in range(len(candidates)):
+        percentages.append(votes[x]/voter_counter)
+    # find the index for the max number of votes of the candidates and with that index, find the winner in the candidates list
     winner = candidates[votes.index(max(votes))]
+    # Joining the data with zip
+    datos = zip(candidates, percentages, votes)
 
-print(f"""Election Results
+# formating the output in 3 parts
+results_header = f"""Election Results
 -----------------------------
 Total votes : {voter_counter:,}
------------------------------""")
-anotherlooper = 0
-for candidate in candidates:
-    print(f"{candidates[anotherlooper]}: {percentages[anotherlooper]:.2%} ({votes[anotherlooper]:,})")
-    anotherlooper +=1
-print(f"""-----------------------------
+-----------------------------
+"""
+ind_result = [f"{info[0]}: {info[1]:.2%} ({info[2]:,})" for info in datos]
+results_footer= f"""
+-----------------------------
 Winner : {winner}
 -----------------------------
-""")
+"""
+# printing the 3 parts
+print(results_header)
+for result in ind_result:
+    print(result)
+print(results_footer)
+
+# Creating the output file
+output_file = open("Election results.txt", "w+")
+#writing the 3 parts into the file
+output_file.write (results_header)
+for result in ind_result:
+    output_file.write(result+"\n")
+output_file.write (results_footer)
